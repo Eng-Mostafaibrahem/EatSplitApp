1e83232bd442fd15c85fc74b937e593dcf8f177c
Eng-Mostafaibrahem <eng.mostafaibrahem99@gmail.com>
2025-02-26 22:15:07 +0200

    add placeHolder

--- src/AddFriend.jsx
+++ src/AddFriend.jsx
@@ -53,6 +53,7 @@ export default function AddFriend({ onAddFriend }) {
         <input
           type="text"
           id="Balance"
+          placeholder="100"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           value={friendBalance}
           onChange={(e) => setFriendBalance(e.target.value)}
@@ -68,6 +69,7 @@ export default function AddFriend({ onAddFriend }) {
         <input
           type="text"
           id="image"
+          placeholder="https://i.pravatar.cc/48"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           value={image}
           onChange={(e) => setImage(e.target.value)}
